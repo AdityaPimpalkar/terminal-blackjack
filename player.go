ae76f6143f82123328ab3f2055e6abf01065009e
aditya pimpalkar <adipimpalkar@gmail.com>
2024-08-10 21:47:50 +0100

    detect ace logic

--- player.go
+++ player.go
@@ -8,15 +8,19 @@ type Player struct {
 }
 
 func (player *Player) GetPoints() int {
-	var aces int
 	cards := player.Cards
 	var totalPoints int
 	for i := 0; i < len(cards); i++ {
 		cardFace := cards[i].Face
 		if cardFace == "Ace" {
-			aces += 1
+			if totalPoints+10 <= 21 {
+				totalPoints += 10
+			} else {
+				totalPoints += 1
+			}
+		} else {
+			totalPoints += cards[i].Point
 		}
-		totalPoints += cards[i].Point
 	}
 	return totalPoints
 }
