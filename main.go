db6f564a83818b8fa96f914a3007c300f36c15b9
aditya pimpalkar <adipimpalkar@gmail.com>
2024-08-12 22:49:08 +0100

    add progress view

--- main.go
+++ main.go
@@ -6,15 +6,26 @@ import (
 	"time"
 
 	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+type ProgressStatus string
+
+type ProgressBarMsg struct {
+	status ProgressStatus
+}
+
 type MainModel struct {
-	betting      Betting
-	game         Game
-	windowWidth  int
-	windowHeight int
+	betting        Betting
+	game           Game
+	progress       progress.Model
+	isProgressView bool
+	phase          int
+	windowWidth    int
+	windowHeight   int
 }
 
 func InitModel() MainModel {
@@ -36,6 +47,8 @@ func InitModel() MainModel {
 			help: help.New(),
 			keys: gameModeKeys,
 		},
+		progress: progress.New(progress.WithScaledGradient("#FF7CCB", "#FDFF8C")),
+		phase:    1,
 	}
 }
 
@@ -48,23 +61,34 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.WindowSizeMsg:
 		m.windowWidth = msg.Width
 		m.windowHeight = msg.Height
+		m.progress.Width = msg.Width - 2*2 - 4
+		if m.progress.Width > 70 {
+			m.progress.Width = 70
+		}
+		return m, nil
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
 			return m, tea.Quit
 		case tea.KeyEnter:
-			if !m.game.isActiveRound && m.betting.Bet > 0 && m.betting.Bet <= m.betting.Balance {
-				m.game.isActiveRound = true
-				m.game.Deck.Init()
-				m.game.Deck.Shuffle()
-				m.game.Player.Bet = m.betting.Bet
-				m.game.Player.Balance = m.betting.Balance - m.betting.Bet
-				err := m.game.DealFirstHand()
-				if err != nil {
-					panic(err)
-				}
-				return m, m.game.Status()
-			}
+			m.isProgressView = true
+			return m, tea.Batch(
+				tea.Tick(time.Millisecond*500, func(t time.Time) tea.Msg {
+					return ProgressBarMsg{
+						status: "loading",
+					}
+				}),
+				tea.Tick(time.Millisecond*1500, func(t time.Time) tea.Msg {
+					return ProgressBarMsg{
+						status: "done",
+					}
+				}),
+				tea.Tick(time.Millisecond*3000, func(t time.Time) tea.Msg {
+					return ProgressBarMsg{
+						status: "started",
+					}
+				}),
+			)
 		case tea.KeySpace:
 			if m.game.isActiveRound && !m.game.playerStood {
 				m.game.PlayerHand()
@@ -82,6 +106,31 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.betting.keys.Enter.SetEnabled(true)
 			m.betting.keys.Reset.SetEnabled(false)
 			return m, nil
+		}
+	case ProgressBarMsg:
+		switch msg.status {
+		case "loading":
+			m.phase++
+			return m, nil
+		case "done":
+			m.phase++
+			return m, nil
+		case "started":
+			m.phase++
+		}
+		if m.phase > 3 {
+			m.isProgressView = false
+			m.phase = 1
+			m.game.isActiveRound = true
+			m.game.Deck.Init()
+			m.game.Deck.Shuffle()
+			m.game.Player.Bet = m.betting.Bet
+			m.game.Player.Balance = m.betting.Balance - m.betting.Bet
+			err := m.game.DealFirstHand()
+			if err != nil {
+				panic(err)
+			}
+			return m, m.game.Status()
 		}
 	case GameStatusMsg:
 		switch msg.status {
@@ -116,7 +165,6 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			time.Sleep(2 * time.Second)
 			m.betting.Balance = m.game.Player.Balance
 			m.betting.Bet = 0
-
 			m.betting.InputBet.Reset()
 			m.game.Reset()
 			return m, nil
@@ -129,9 +177,35 @@ func (m MainModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
+func ProgressView(m MainModel) string {
+	var msg string
+	switch m.phase {
+	case 1:
+		msg = "Shuffling cards..."
+	case 2:
+		msg = "Starting round..."
+	case 3:
+		msg = "Good luck!"
+	}
+
+	return lipgloss.Place(
+		m.windowWidth,
+		m.windowHeight,
+		lipgloss.Position(lipgloss.Center),
+		lipgloss.Position(lipgloss.Center),
+		lipgloss.JoinVertical(
+			lipgloss.Center,
+			lipgloss.NewStyle().Bold(true).Render(msg+"\n"),
+			m.progress.ViewAs(float64(m.phase)/3.0),
+		),
+	)
+}
+
 func (m MainModel) View() string {
 	if m.game.isActiveRound {
 		return GameView(m)
+	} else if m.isProgressView {
+		return ProgressView((m))
 	}
 	return BettingView(m)
 }
